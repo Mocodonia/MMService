0cf8752e4aa33034643d7d561b083f87f5b4c8fb
7Zeb <66693249+7zeb@users.noreply.github.com>
2025-09-28 20:30:58 -0500

    fix main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const map = L.map('map').setView([0, 0], 2); // Placeholder coords
 
-L.tileLayer('./tiles/{z}/{x}/{y}.png', {
+L.tileLayer('/tiles/{z}/{x}/{y}.png', {
   attribution: 'MMService • Mocodonia',
   maxZoom: 18,
 }).addTo(map);
