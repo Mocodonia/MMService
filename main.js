fe0e13c71131a7e7185f35fd32daa7ade021b218
7Zeb <66693249+7zeb@users.noreply.github.com>
2025-09-30 07:34:51 -0500

    I don't know if this will fix anything

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const map = L.map('map').setView([0, 0], 2); //Set view to center for now
 
 //1. FIX: Define the tile layer using the URL string with standard Leaflet placeholders {x}, {y}, {z}
-const mocodoniaTilesURL = '/MMService/tiles/{x},{y}.png'; //Use {x} and {y} in the URL template
+const mocodoniaTilesURL = '/MMService/tiles/{z}/{x},{y}.png'; //this will now include Z so that zoom will be set
 
 //2. FIX: Use the standard L.tileLayer factory method
 L.tileLayer(mocodoniaTilesURL, {
